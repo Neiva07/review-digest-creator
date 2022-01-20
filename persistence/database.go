70a8b1a0a655bf43820f9a37a9089b5f7e730398
Lucas Neiva <lucasneiva@Lucass-MacBook-Air.local>
2022-01-20 09:21:42 -0300

    initial commit - creating database instance

---
+++ persistence/database.go
@@ -0,0 +1,32 @@
+package persistence
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func init() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+}
